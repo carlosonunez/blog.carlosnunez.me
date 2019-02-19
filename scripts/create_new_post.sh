d3c5da6f61ff4a7844f03365021e237934023bda
Carlos Nunez <carlosonunez@noreply.github.com>
2019-02-18 22:18:13 -0500

    Add a script that creates a post for me so that I'm not copy pasting from older posts all of the time. Technically I could use 'hugo generate' for this purpose, but my post format is boilerplate enough for this approach to work fine.

---
+++ scripts/create_new_post.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env sh
+
+if [ "$VERBOSE_MODE" == "true" ]
+then
+  set -x
+fi
+
+usage() {
+  cat <<-USAGE
+create_new_post.sh [post-name]
+
+Creates a new blog post with default Hugo front matter.
+
+For additional verbosity, add "VERBOSE_MODE=true" to the command.
+USAGE
+}
+
+_get_post_slug() {
+  post_name="$1"
+  echo "$post_name" | \
+    tr '[:upper:]' '[:lower:]' | \
+    tr ' ' '-' | \
+    tr -dc '[:alnum:]\n\r-'
+}
+
+ensure_directories_present() {
+  test -d /content && \
+  test -d /images
+}
+
+create_images_dir_for_post() {
+  post_name=$1
+  post_slug=$(_get_post_slug "$post_name")
+  mkdir "/images/$post_slug"
+}
+
+create_new_post_with_front_matter() {
+  post_name="$1"
+  post_slug=$(_get_post_slug "$post_name")
+  todays_date=$(date +"%Y-%m-%d %H:%M:%S")
+  cat >"/content/${post_slug}.md" <<-POST
+---
+title: "$post_name"
+date: "$todays_date"
+draft: true
+slug: "$post_slug"
+image: "/images/$post_slug/header.jpg"
+keywords:
+  - enterprise
+  - strategy
+  - digital transformation
+  - add some more keywords and stuff
+---
+POST
+}
+
+if ! ensure_directories_present
+then
+  >&2 echo "ERROR: /content or /images should be mounted."
+  exit 1
+fi
+
+post_name="$1"
+if test -z "$post_name"
+then
+  usage
+  >&2 echo "ERROR: Missing post name."
+  exit 1
+fi
+
+if ! {
+  create_images_dir_for_post "$post_name" &&
+  create_new_post_with_front_matter "$post_name" ;
+}
+then
+  >&2 echo "ERROR: Failed to create new post."
+  exit 1
+fi
